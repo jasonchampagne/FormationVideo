f451c025eb07f4253410483b74239d21132ad878
Jason CHAMPAGNE <979506+jasonchampagne@users.noreply.github.com>
2021-03-01 15:57:08 +0100

    Update main.c

--- Ressources/C/exercices/10/correction/main.c
+++ Ressources/C/exercices/10/correction/main.c
@@ -3,5 +3,48 @@
 
 int main(void)
 {
+	FILE *datafile = fopen("data_big.txt", "r");
+
+	if(datafile == NULL)
+		exit(1);
+
+	FILE *potifile = fopen("data_big.poti", "w");
+
+	if(potifile == NULL)
+		exit(1);
+
+	//-----------------------------------------------------------
+
+	char character, last_character;
+	int count = 0;
+
+	printf("Compression en cours...");
+
+	if((last_character = fgetc(datafile)) != EOF)
+	{
+		count++;
+
+		while((character = fgetc(datafile)) != EOF)
+		{
+			if(character == last_character)
+				count++;
+			else
+			{
+				fprintf(potifile, "%c%d", last_character, count);
+				last_character = character;
+				count = 1;
+			}
+		}
+
+		fprintf(potifile, "%c%d", last_character, count);
+	}
+
+	printf("OK\n");
+
+	//-----------------------------------------------------------
+	
+	fclose(datafile);
+	fclose(potifile);
+
 	return 0;
 }

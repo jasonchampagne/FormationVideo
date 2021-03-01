5b8794ba0dbef5bec5fc5ceb449163c26bbca469
Jason CHAMPAGNE <979506+jasonchampagne@users.noreply.github.com>
2021-03-01 01:40:10 +0100

    Update code.c

--- Ressources/C/exercices/10/code.c
+++ Ressources/C/exercices/10/code.c
@@ -6,5 +6,23 @@
 
 int main(void)
 {
+	// Chargement et allocation des ressources
+	//...
+	
+	// Parcours du fichier et remplissage du tableau
+	//...
+	
+	// Affichage
+	for(i = 0 ; i < rows ; i++)
+	{
+		for(j = 0 ; j < columns ; j++)
+			printf("%c ", matrix[i][j]);
+
+		printf("\n");
+	}
+	
+	// Libération des ressources
+	//...
+	
 	return 0;
 }
